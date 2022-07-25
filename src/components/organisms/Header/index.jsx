574785194f65c4681989f453fa2c3fbbccabdc08
KenjiLucas <802.182@alunos.unigran.br>
2022-07-25 14:31:03 -0400

    style/ style header and the burguer button

--- src/components/organisms/Header/index.jsx
+++ src/components/organisms/Header/index.jsx
@@ -5,7 +5,9 @@ const Header = () => {
     <header className="header">
       <img src="/assets/shared/logo.svg" alt="" className="header__logo" />
       <HeaderNav />
-      <button className="header__burguer">burguer</button>
+      <button className="header__burguer">
+        <span className="header__line"></span>
+      </button>
     </header>
   );
 };
