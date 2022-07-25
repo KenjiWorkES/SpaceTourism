94123e08949a78d41704175406247adda3410bd7
KenjiLucas <802.182@alunos.unigran.br>
2022-07-25 13:56:01 -0400

    feature/ create basic jsx for Header

--- src/components/organisms/Header/index.jsx
+++ src/components/organisms/Header/index.jsx
@@ -1,3 +1,13 @@
-const Header = () => {};
+import { HeaderNav } from "../../molecules";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="/assets/shared/logo.svg" alt="" className="header__logo" />
+      <HeaderNav />
+      <button className="header__burguer">burguer</button>
+    </header>
+  );
+};
 
 export default Header;
