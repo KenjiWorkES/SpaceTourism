2b20b522cdde816e8235ed3d52e965eee7911c0d
KenjiLucas <802.182@alunos.unigran.br>
2022-07-25 14:09:29 -0400

    feature/ create basic jsx for HeaderItem

--- src/components/atoms/HeaderItem/index.jsx
+++ src/components/atoms/HeaderItem/index.jsx
@@ -1,3 +1,14 @@
-const HeaderItem = () => {};
+import { NavLink } from "react-router-dom";
+
+const HeaderItem = () => {
+  return (
+    <li className="header__item">
+      <NavLink className="header__link" to="/">
+        <label className="header__label">00</label>
+        <p className="header__text">Home</p>
+      </NavLink>
+    </li>
+  );
+};
 
 export default HeaderItem;
