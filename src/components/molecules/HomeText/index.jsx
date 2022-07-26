89221c14c85215eebf6517781ff6e8865b3752a5
KenjiLucas <802.182@alunos.unigran.br>
2022-07-26 15:05:13 -0400

    style/ style the title and subtitle for home

--- src/components/molecules/HomeText/index.jsx
+++ src/components/molecules/HomeText/index.jsx
@@ -3,8 +3,10 @@ import { Paragraph } from "../../atoms/";
 const HomeText = () => {
   return (
     <div className="home__text">
-      <h2 className="title title--smallest">SO, YOU WANT TO TRAVEL TO</h2>
-      <h1 className="title title--largest">Space</h1>
+      <h2 className="title title--smallest home__subTitle">
+        SO, YOU WANT TO TRAVEL TO
+      </h2>
+      <h1 className="title title--largest home__title">Space</h1>
       <Paragraph>
         Let’s face it; if you want to go to space, you might as well genuinely
         go to outer space and not hover kind of on the edge of it. Well sit
