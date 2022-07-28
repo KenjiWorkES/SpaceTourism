319713fefbe44de9a9e57fc7109948290e425726
KenjiLucas <802.182@alunos.unigran.br>
2022-07-28 14:01:13 -0400

    feature/ add content to DestinationItem

--- src/components/molecules/DestinationControl/index.jsx
+++ src/components/molecules/DestinationControl/index.jsx
@@ -1,3 +1,14 @@
-const DestinationControl = () => {};
+import { DestinationItem } from "../../atoms";
+
+const DestinationControl = () => {
+  return (
+    <ul className="destination__controler">
+      <DestinationItem />
+      <DestinationItem />
+      <DestinationItem />
+      <DestinationItem />
+    </ul>
+  );
+};
 
 export default DestinationControl;
