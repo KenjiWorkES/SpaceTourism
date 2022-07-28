0c1b3302b1dbc8a0a11216c263374eca9a79fc38
KenjiLucas <802.182@alunos.unigran.br>
2022-07-28 13:04:42 -0400

    feature/ create routes in app to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import "./App.scss";
+import { Route, Routes } from "react-router-dom";
 
-import { HomePage } from "../src/components/pages";
+import {
+  HomePage,
+  DestinationPage,
+  CrewPage,
+  TechnologyPage,
+} from "../src/components/pages";
 import Layout from "./layout";
 
 function App() {
   return (
     <Layout>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/destination" element={<DestinationPage />} />
+        <Route path="/crew" element={<CrewPage />} />
+        <Route path="/technology" element={<TechnologyPage />} />
+      </Routes>
     </Layout>
   );
 }
