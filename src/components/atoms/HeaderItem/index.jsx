77a0c64f9f4b0d7dffcfcd244cb9a7ad706a1534
KenjiLucas <802.182@alunos.unigran.br>
2022-07-28 13:05:55 -0400

    feature/ place the right links in navigation

--- src/components/atoms/HeaderItem/index.jsx
+++ src/components/atoms/HeaderItem/index.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const HeaderItem = ({ index, text, link }) => {
   return (
     <li className="header__item">
-      <NavLink className="header__link" to={index}>
+      <NavLink className="header__link" to={link}>
         <label className="header__label">{index}</label>
         <p className="header__text">{text}</p>
       </NavLink>
